f9779db839345a1a87957b1919705700ddb66b7d
Iulian <iparaian@gmail.com>
2017-02-20 16:08:29 +0000

    upgrades and fixes
    - upgrade csproj
    - fix json respons for about and health endpoints
    - fix name for Build-info

--- src/Utilitywarehouse.OpsEndpoints/Owin/OpsEndpointsMiddleware.cs
+++ src/Utilitywarehouse.OpsEndpoints/Owin/OpsEndpointsMiddleware.cs
@@ -21,14 +21,16 @@ namespace Utilitywarehouse.OpsEndpoints.Owin
         private readonly RequestDelegate _next;
         private readonly JsonSerializer _serializer;
         private readonly Dictionary<string, HttpHandler> _handlers;
+        private readonly JsonSerializerSettings _serializerSettings;
 
         public OpsEndpointsMiddleware(RequestDelegate next, OpsEndpointsMiddlewareOptions options)
         {
             _next = next;
             _handlers = ConfigureHandlers(options);
-            var serializer = new JsonSerializer {ContractResolver = new CamelCasePropertyNamesContractResolver()};
-            serializer.Converters.Add(new StringEnumConverter());
-            _serializer = serializer;
+            _serializerSettings = new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented
+            };
         }
 
         private Dictionary<string, HttpHandler> ConfigureHandlers(OpsEndpointsMiddlewareOptions options)
@@ -54,11 +56,8 @@ namespace Utilitywarehouse.OpsEndpoints.Owin
                 AboutResponse response = op.HealthModel.About().ToAboutResponse();
                 context.Response.StatusCode = 200;
                 context.Response.ContentType = "application/json";
-                using (var writer = new StreamWriter(context.Response.Body, Encoding.UTF8))
-                {
-                    _serializer.Serialize(writer, response);
-                    return writer.FlushAsync();
-                }
+
+                return context.Response.WriteAsync(JsonConvert.SerializeObject(response, _serializerSettings));
             });
 
             var health = new HttpHandler(context =>
@@ -67,11 +66,8 @@ namespace Utilitywarehouse.OpsEndpoints.Owin
                 HealthResponse response = op.HealthModel.Health().ToHealthResponse();
                 context.Response.StatusCode = 200;
                 context.Response.ContentType = "application/json";
-                using (var writer = new StreamWriter(context.Response.Body, Encoding.UTF8))
-                {
-                    _serializer.Serialize(writer, response);
-                    return writer.FlushAsync();
-                }
+
+                return context.Response.WriteAsync(JsonConvert.SerializeObject(response, _serializerSettings));
             });
 
             return new Dictionary<string, HttpHandler>
@@ -84,9 +80,12 @@ namespace Utilitywarehouse.OpsEndpoints.Owin
 
         public Task Invoke(HttpContext context)
         {
-            if (context.Request.Path.StartsWithSegments("/__", out var _, out var segment))
+            PathString segment = null;
+
+            if (context.Request.Path.StartsWithSegments("/__", out segment))
             {
                 Func<HttpContext, Task> handler;
+                
                 if (_handlers.TryGetValue(segment, out handler))
                 {
                     return handler.Invoke(context);
